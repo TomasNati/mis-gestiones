a2014fa7ac64b297caefd7ece7f36e6ea0334bd0
Andres Asteasuain <andres.asteasuain.contractor@bydeluxe.com>
2023-12-01 09:50:19 -0300

    Agregando Material UI

---
+++ src/components/ThemeRegistry/theme.ts
@@ -0,0 +1,40 @@
+import { Roboto } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#9ba4d4',
+    },
+    secondary: {
+      main: '#42a5f5',
+    },
+    background: {
+      default: '#101414',
+      paper: '#464e52',
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+  components: {
+    MuiAlert: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.severity === 'info' && {
+            backgroundColor: '#60a5fa',
+          }),
+        }),
+      },
+    },
+  },
+});
+
+export default theme;
