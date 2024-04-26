91d24408312a3f5954a3afa8d22818160fd996e8
Andres Asteasuain <andres.asteasuain.contractor@bydeluxe.com>
2024-04-26 14:42:12 -0300

    Correcciones en el cálculo del total acumulado de gastos en el mes

--- src/components/graficos/CrecimientoDeGastosEnElMes.tsx
+++ src/components/graficos/CrecimientoDeGastosEnElMes.tsx
@@ -1,4 +1,5 @@
 import { MovimientoGastoGrilla } from '@/lib/definitions';
+import { transformNumberToCurrenty } from '@/lib/helpers';
 import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line, ReferenceLine } from 'recharts';
 
 type GastoPorDia = {
@@ -20,36 +21,36 @@ const CrecimientoDeGastosEnElMes = ({
     return diaA - diaB;
   });
 
-  let totalAcumulado = 0;
   const gastosAcumuladosPorDia: GastoPorDia[] = sortedMovimientos.reduce((acc: GastoPorDia[], movimiento) => {
     const dia = new Date(movimiento.fecha).getDate();
     const monto = movimiento.monto;
-    totalAcumulado += monto;
 
     if (acc.length === 0) {
-      acc.push({ dia, totalPorDia: monto, totalAcumulado: monto });
+      acc.push({ dia, totalPorDia: monto, totalAcumulado: 0 });
     } else {
       const gastosDelDia = acc.find((gasto) => gasto.dia === dia);
       if (gastosDelDia) {
         gastosDelDia.totalPorDia += monto;
-        gastosDelDia.totalAcumulado += monto;
       } else {
-        totalAcumulado += monto;
-        acc.push({ dia, totalPorDia: monto, totalAcumulado });
+        acc.push({ dia, totalPorDia: monto, totalAcumulado: 0 });
       }
     }
 
     return acc;
   }, []);
 
-  console.log(totalEstimado);
+  gastosAcumuladosPorDia.forEach((gasto, index) => {
+    gasto.totalAcumulado = gastosAcumuladosPorDia
+      .slice(0, index + 1) //obtengo todos los dias hasta el actual
+      .reduce((acc, gasto) => acc + gasto.totalPorDia, 0); //sumo los gastos de cada dia
+  });
 
   return (
     <LineChart width={700} height={300} data={gastosAcumuladosPorDia}>
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="dia" />
       <YAxis />
-      <Tooltip />
+      <Tooltip formatter={(value: number) => transformNumberToCurrenty(value)} />
       <Legend />
       <Line type="monotone" dataKey="totalAcumulado" stroke="#8884d8" name="Acumulado" />
       <Line type="monotone" dataKey="totalPorDia" stroke="#82ca9d" name="Gastos del día" />
