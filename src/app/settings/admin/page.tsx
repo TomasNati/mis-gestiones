86fa896e6bd3a6516f98448661fadf7d09df1867
Andres Asteasuain <andres.asteasuain@hotmail.com>
2026-05-06 15:03:16 -0300

    Ajustando altura de contenedor

--- src/app/settings/admin/page.tsx
+++ src/app/settings/admin/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const EmbeddedSite: React.FC = () => {
   return (
-    <div style={{ width: '100%', height: '100vh' }}>
+    <div style={{ width: '100%', height: '100%' }}>
       <iframe
         src="https://mis-gestiones-admin.vercel.app/"
         title="Mis gestiones - Admin"
